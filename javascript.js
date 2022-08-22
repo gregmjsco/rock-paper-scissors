5d3f98dc3e65c3551d8d760285977cc81244a9bf
GregMJ <gregmj.sco@gmail.com>
2022-08-22 13:07:20 +0900

    Add playRound() and work as intended. Player not able to choose

--- javascript.js
+++ javascript.js
@@ -6,10 +6,34 @@ function random(number) {
 function getComputerChoice() {
      let choice = random(3);
     if (choice == 0) {
-        return "Rock"
+        return "rock"
     } else if (choice == 1) {
-        return "Paper"
+        return "paper"
     } else {
-        return "Scissors"
+        return "scissors"
     }
 }
+
+
+
+function playRound(playerSelection, computerSelection) {
+    // your code here!
+    //Compare players selection to computers selection 
+    //If player selected rock and computer selected rock display appropriate message
+    // check for all other potential cases
+
+    if (playerSelection =="rock" && computerSelection == "rock"){
+        //console.log("Tie! Try again!");
+        return "Tie! Try again!"
+    } else if (playerSelection =="rock" && computerSelection == "scissors"){
+        //console.log("You win! Rock beats scissors.");
+        return "You win! Rock beats scissors."
+    } else if (playerSelection == "rock" && computerSelection == "paper") {
+        //console.log("You lose! Paper beats rock.")
+        return "You lose! Paper beats rock."
+    }
+  }
+   
+  const playerSelection = "rock";
+  const computerSelection = getComputerChoice();
+  console.log(playRound(playerSelection, computerSelection));
