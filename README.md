103d51d12abda0f268e14bfd413eb08eff7381b3
Greg Johnston <gregmj.sco@gmail.com>
2022-09-02 18:00:46 +0900

    Update index and javascript to second implementation

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # rock-paper-scissors
 Odin Project Javascript Rock-Scissor-Paper project
+
+Second attempt after issues with first implementation. Reread Javascript resources and did more practice excercises before attempting project again.
--- javascript.js
+++ javascript.js
@@ -1,3 +1,72 @@
+const computerChoiceDisplay = document.getElementById("computer-choice");
+const playerChoiceDisplay = document.getElementById("player-choice");
+const resultDisplay = document.getElementById("result");
+const possibleChoices = document.querySelectorAll("button");
+const playerScoreDisplay = document.getElementById("player-score")
+const computerScoreDisplay = document.getElementById("computer-score")
+let playerChoice
+let computerChoice
+let result
+let playerScore = 0
+let computerScore = 0
+
+
+possibleChoices.forEach(possibleChoices => possibleChoices.addEventListener("click", (e) => {
+    playerChoice = e.target.id
+    playerChoiceDisplay.innerHTML = playerChoice
+    generateComputerChoice()
+    game()
+}))
+
+function playRound(playerChoice, computerChoice) {
+    if (playerChoice === computerChoice) {
+        result = "Round is a draw"
+    } else if (playerChoice === "rock" && computerChoice === "scissors") {
+        result = "You win"
+    } else if (playerChoice === "rock" && computerChoice === "paper") {
+        result = "You lose"
+    } else if (playerChoice === "paper" && computerChoice == "rock") {
+        result = "You win"
+    } else if (playerChoice === "paper" && computerChoice == "scissors") {
+        result = "You lose"
+    } else if (playerChoice === "scissors" && computerChoice == "paper") {
+        result = "You win"
+    } else if (playerChoice === "scissors" && computerChoice == "rock") {
+        result = "You lose"
+    }
+    resultDisplay.innerHTML = result 
+}
+
+
+function generateComputerChoice() {
+    const randomNumber = Math.floor(Math.random() * possibleChoices.length) + 1
+
+    if (randomNumber === 1) {
+        computerChoice = "rock"
+    } else if (randomNumber === 2) {
+        computerChoice = "paper"
+    } else if (randomNumber === 3) {
+        computerChoice = "scissors"
+    }
+
+    computerChoiceDisplay.innerHTML = computerChoice
+}
+
+function updateScores(){
+    if (result === "You win"){
+        playerScoreDisplay.innerHTML = ++playerScore
+    } else if (result === "You lose") {
+        computerScoreDisplay.innerHTML = ++computerScore
+    }
+}
+
+function game(){
+    for (let i = 0; i < 5; i++) {
+        playRound(playerChoice, computerChoice)
+        updateScores()
+     }
+}
+/*
 const playerSelection = getPlayerChoice();
 const computerSelection = getComputerChoice();
 
@@ -37,7 +106,7 @@ let sayWin = () => console.log("Round is a win");
 let sayLose = () => console.log("Round is a loss");
 
 
-
+*/
 
 
 
